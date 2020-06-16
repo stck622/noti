dbfc3f2165450073446dfd6aec3a2fe4731069de
stck622 <stck622@naver.com>
2020-06-16 15:57:29 +0900

    complete

---
+++ app/src/main/java/kr/fire/noti/fbData.java
@@ -0,0 +1,12 @@
+package kr.fire.noti;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class fbData {
+
+    fbData(){};
+
+    public Map<String, Map<String,String>> gear = new HashMap<>();
+
+}
